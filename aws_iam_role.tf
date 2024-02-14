addd51df7d1f549f6e7f093f240bf79c88f97692
Iago Ferreira <iago.ferreira@ops.team>
2024-02-14 09:31:00 -0300

    ajust module

--- aws_iam_role.tf
+++ aws_iam_role.tf
@@ -3,59 +3,154 @@
 resource "aws_iam_role" "metric_stream_to_firehose" {
   name               = "metric_stream_to_firehose_role"
   assume_role_policy = data.aws_iam_policy_document.streams_assume_role.json
+  inline_policy {
+    name = "my_inline_policy"
+
+    policy = jsonencode({
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Effect": "Allow",
+              "Action": [
+                  "firehose:PutRecord",
+                  "firehose:PutRecordBatch"
+              ],
+              "Resource": [
+                  "arn:aws:firehose:us-east-1:*:deliverystream/cloudwatch-metrics-to-otel"
+              ]
+          }
+      ]
+    })
+  }
 }
-
-# data "aws_iam_policy_document" "metric_stream_to_firehose" {
-#   statement {
-#     effect = "Allow"
-#     actions = [
-#       "firehose:PutRecord",
-#       "firehose:PutRecordBatch",
-#     ]
-#     resources = [aws_kinesis_firehose_delivery_stream.https.arn]
-#   }
-# }
-
-# resource "aws_iam_role_policy" "metric_stream_to_firehose" {
-#   name   = "default"
-#   role   = aws_iam_role.metric_stream_to_firehose.id
-#   policy = data.aws_iam_policy_document.metric_stream_to_firehose.json
-# }
-
-
 
 resource "aws_iam_role" "firehose_to_s3" {
   assume_role_policy = data.aws_iam_policy_document.firehose_assume_role.json
 }
 
-
 resource "aws_iam_role_policy" "firehose_to_s3" {
   name   = "default"
   role   = aws_iam_role.firehose_to_s3.id
-  policy = data.aws_iam_policy_document.firehose_to_s3.json
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Action": [
+                "glue:GetTable",
+                "glue:GetTableVersion",
+                "glue:GetTableVersions"
+            ],
+            "Resource": [
+                "arn:aws:glue:us-east-1:*:catalog",
+                "arn:aws:glue:us-east-1:*:database/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%",
+                "arn:aws:glue:us-east-1:*:table/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%"
+            ]
+        },
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Action": [
+                "glue:GetSchemaByDefinition"
+            ],
+            "Resource": [
+                "arn:aws:glue:us-east-1:*:registry/*",
+                "arn:aws:glue:us-east-1:*:schema/*"
+            ]
+        },
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Action": [
+                "glue:GetSchemaVersion"
+            ],
+            "Resource": [
+                "*"
+            ]
+        },
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Action": [
+                "s3:*"
+            ],
+            "Resource": [
+                "${data.aws_s3_bucket.bucket.arn}",
+                "${data.aws_s3_bucket.bucket.arn}/*"
+            ]
+        },
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Action": [
+                "lambda:InvokeFunction",
+                "lambda:GetFunctionConfiguration"
+            ],
+            "Resource": "arn:aws:lambda:us-east-1:*:function:%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "kms:GenerateDataKey",
+                "kms:Decrypt"
+            ],
+            "Resource": [
+                "arn:aws:kms:us-east-1:*:key/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%"
+            ],
+            "Condition": {
+                "StringEquals": {
+                    "kms:ViaService": "s3.us-east-1.amazonaws.com"
+                },
+                "StringLike": {
+                    "kms:EncryptionContext:aws:s3:arn": [
+                        "arn:aws:s3:::%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%/*",
+                        "arn:aws:s3:::%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%"
+                    ]
+                }
+            }
+        },
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Action": [
+                "logs:PutLogEvents"
+            ],
+            "Resource": [
+                "arn:aws:logs:us-east-1:*:log-group:/aws/kinesisfirehose/*:log-stream:*",
+                "arn:aws:logs:us-east-1:*:log-group:%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%:log-stream:*"
+            ]
+        },
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Action": [
+                "kinesis:DescribeStream",
+                "kinesis:GetShardIterator",
+                "kinesis:GetRecords",
+                "kinesis:ListShards"
+            ],
+            "Resource": "arn:aws:kinesis:us-east-1:*:stream/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "kms:Decrypt"
+            ],
+            "Resource": [
+                "arn:aws:kms:us-east-1:*:key/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%"
+            ],
+            "Condition": {
+                "StringEquals": {
+                    "kms:ViaService": "kinesis.us-east-1.amazonaws.com"
+                },
+                "StringLike": {
+                    "kms:EncryptionContext:aws:kinesis:arn": "arn:aws:kinesis:us-east-1:*:stream/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%"
+                }
+            }
+        }
+    ]
 }
 
-
-# resource "aws_kinesis_firehose_delivery_stream" "https" {
-#   name        = "otel-promemetheus"
-#   destination = "http_endpoint"
-#   http_endpoint_configuration {
-#     url                = "https://otel-test.ops.team"
-#     name               = "HTTPS endpoint opentelemetry"
-#     access_key         = "SimplesVet"
-#     buffering_size     = 15
-#     buffering_interval = 300
-#     role_arn           = aws_iam_role.firehose_to_s3.arn
-#     s3_backup_mode     = "FailedDataOnly"
-#     s3_configuration {
-#       role_arn           = aws_iam_role.firehose_to_s3.arn
-#       bucket_arn         = aws_s3_bucket.bucket.arn
-#       buffering_size     = 10
-#       buffering_interval = 400
-#       compression_format = "GZIP"
-#     }
-#     request_configuration {
-#       content_encoding = "GZIP"
-#     }
-#   }
-# }
+  )
+}
--- data.tf
+++ data.tf
@@ -24,20 +24,17 @@ data "aws_iam_policy_document" "firehose_assume_role" {
   }
 }
 
-data "aws_iam_policy_document" "firehose_to_s3" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "s3:AbortMultipartUpload",
-      "s3:GetBucketLocation",
-      "s3:GetObject",
-      "s3:ListBucket",
-      "s3:ListBucketMultipartUploads",
-      "s3:PutObject",
-    ]
-    resources = [
-      data.aws_s3_bucket.bucket.arn,
-      "${data.aws_s3_bucket.bucket.arn}/*",
-    ]
-  }
-}
+# data "aws_iam_policy_document" "firehose_to_s3" {
+#   statement {
+#     effect = "Allow"
+#     actions = [
+#         "glue:GetTable",
+#         "glue:GetTableVersion",
+#         "glue:GetTableVersions"
+#     ]
+#     resources = [
+#       data.aws_s3_bucket.bucket.arn,
+#       "${data.aws_s3_bucket.bucket.arn}/*",
+#     ]
+#   }
+# }
